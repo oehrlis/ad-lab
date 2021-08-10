7c17c201445c3dd95695a6b0c8922e8fed0923dc
Stefan Oehrli <stefan.oehrli@trivadis.com>
2021-08-10 15:26:35 +0200

    disable password history

--- scripts/01_install_ad_role.ps1
+++ scripts/01_install_ad_role.ps1
@@ -67,6 +67,7 @@ if ((gwmi win32_computersystem).partofdomain -eq $false) {
     # Disable password complexity policy
     secedit /export /cfg C:\secpol.cfg
     (Get-Content C:\secpol.cfg).replace("PasswordComplexity = 1", "PasswordComplexity = 0") | Out-File C:\secpol.cfg
+    (Get-Content C:\secpol.cfg).replace("PasswordHistorySize = 24", "PasswordHistorySize = 0") | Out-File C:\secpol.cfg
     secedit /configure /db C:\Windows\security\local.sdb /cfg C:\secpol.cfg /areas SECURITYPOLICY
     rm -force C:\secpol.cfg -confirm:$false
 
